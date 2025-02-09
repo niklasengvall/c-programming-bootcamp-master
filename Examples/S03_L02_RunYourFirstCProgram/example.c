77533e387b47a70755e115932840a8752cdf2074
Niklas Engvall <niklas_engvall@hotmail.com>
2025-02-09 14:57:52 +0100

    New settings

--- Examples/S03_L02_RunYourFirstCProgram/example.c
+++ Examples/S03_L02_RunYourFirstCProgram/example.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
 {
     int number = 10000;
-    printf("Hello world!\n");
+    printf("Hello world! Number is: %d\n", number);
     return EXIT_SUCCESS;
 }
